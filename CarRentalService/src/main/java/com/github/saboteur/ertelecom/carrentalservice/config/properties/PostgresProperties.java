7e59c2e7469eec422307c0874fb0bfc06d4dbefe
5aboteur <5aboteur@protonmail.com>
2020-05-23 18:23:13 +0700

    Del useless imports

--- CarRentalService/src/main/java/com/github/saboteur/ertelecom/carrentalservice/config/properties/PostgresProperties.java
+++ CarRentalService/src/main/java/com/github/saboteur/ertelecom/carrentalservice/config/properties/PostgresProperties.java
@@ -1,13 +1,9 @@
 package com.github.saboteur.ertelecom.carrentalservice.config.properties;
 
 import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
-
-import javax.sql.DataSource;
 
 @Primary
 @Component
