2da205d3839790f3d2adff0438aa816da02fc2ab
5aboteur <5aboteur@protonmail.com>
2020-05-30 23:01:46 +0700

    Add controller

---
+++ CarRentalService/src/main/java/com/github/saboteur/ertelecom/carrentalservice/controller/CarRentalControllerApi.java
@@ -0,0 +1,72 @@
+package com.github.saboteur.ertelecom.carrentalservice.controller;
+
+import com.github.saboteur.ertelecom.carrentalservice.dto.OrganizationDto;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(value = "Car Rental service controller")
+@RestController
+@RequestMapping("api")
+public interface CarRentalControllerApi {
+
+    // Rent APIs
+
+//    @ApiOperation(value = "")
+//    @PostMapping("")
+//    ResponseEntity<String> createRentRecord(
+//        @ApiParam("Full rental info")
+//        @RequestBody
+//        RentRecordDto rentRecordDto
+//    );
+
+    // Organization APIs
+
+    @ApiOperation(value = "Add organization info")
+    @PostMapping("/carrental/organization/create")
+    ResponseEntity<Long> createOrganization(
+        @ApiParam("Full organization info")
+        @RequestBody
+        OrganizationDto organization
+    );
+
+    @ApiOperation(value = "Get organization info by ID")
+    @GetMapping("/carrental/organization/get")
+    ResponseEntity<OrganizationDto> getOrganization(
+        @ApiParam(value = "Organization ID", required = true)
+        @RequestParam(value = "organizationId")
+        Long organizationId
+    );
+
+    @ApiOperation(value = "Update organization info")
+    @PutMapping("/carrental/organization/update")
+    ResponseEntity<Boolean> updateOrganization(
+        @ApiParam(value = "Organization ID", required = true)
+        @RequestParam(value = "organizationId")
+        Long organizationId,
+
+        @ApiParam("Full organization info")
+        @RequestBody
+        OrganizationDto organization
+    );
+
+    @ApiOperation(value = "Delete organization info")
+    @DeleteMapping("/carrental/organization/delete")
+    ResponseEntity<Boolean> deleteOrganization(
+        @ApiParam(value = "Organization ID", required = true)
+        @RequestParam(value = "organizationId")
+        Long organizationId
+    );
+
+    // Car APIs
+
+}
---
+++ CarRentalService/src/main/java/com/github/saboteur/ertelecom/carrentalservice/controller/impl/CarRentalControllerImpl.java
@@ -0,0 +1,53 @@
+package com.github.saboteur.ertelecom.carrentalservice.controller.impl;
+
+import com.github.saboteur.ertelecom.carrentalservice.controller.CarRentalControllerApi;
+import com.github.saboteur.ertelecom.carrentalservice.dto.OrganizationDto;
+import com.github.saboteur.ertelecom.carrentalservice.service.CarRentalService;
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api
+@RestController
+public class CarRentalControllerImpl implements CarRentalControllerApi {
+
+    @Autowired
+    private CarRentalService carRentalService;
+
+    @Override
+    public ResponseEntity<Long> createOrganization(OrganizationDto organizationDto) {
+        Long createdId = carRentalService.createOrganization(organizationDto);
+        return ResponseEntity
+            .status(
+                createdId == 0
+                    ? HttpStatus.BAD_REQUEST
+                    : HttpStatus.CREATED
+            )
+            .body(createdId);
+    }
+
+    @Override
+    public ResponseEntity<OrganizationDto> getOrganization(Long organizationId) {
+        OrganizationDto result = carRentalService.getOrganization(organizationId);
+        return ResponseEntity
+            .status(
+                result == null
+                    ? HttpStatus.NOT_FOUND
+                    : HttpStatus.OK
+            )
+            .body(result);
+    }
+
+    @Override
+    public ResponseEntity<Boolean> updateOrganization(Long organizationId, OrganizationDto organization) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Boolean> deleteOrganization(Long organizationId) {
+        return null;
+    }
+
+}
