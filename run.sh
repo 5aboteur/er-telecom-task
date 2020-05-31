4c0e60d84b437c5c422ba9661f25a1fa70102ba4
5aboteur <5aboteur@protonmail.com>
2020-05-31 17:34:11 +0700

    Add docker configs

---
+++ run.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+echo -ne "Check for openjdk package... "
+
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' openjdk-8-jre | grep "install ok installed")
+
+if [ "" == "$PKG_OK" ]; then
+	echo "the package wasn't found in the system. Abort."
+	exit 1
+fi
+
+echo "OK."
+
+echo -ne "Check for maven package... "
+
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' maven | grep "install ok installed")
+
+if [ "" == "$PKG_OK" ]; then
+	echo "the package wasn't found in the system. Abort."
+	exit 1
+fi
+
+echo "OK."
+
+echo -ne "Check for docker package... "
+
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' docker | grep "install ok installed")
+
+if [ "" == "$PKG_OK" ]; then
+	echo "the package wasn't found in the system. Abort."
+	exit 1
+fi
+
+echo "OK."
+
+echo -ne "Check for docker-compose package... "
+
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' docker-compose | grep "install ok installed")
+
+if [ "" == "$PKG_OK" ]; then
+	echo "the package wasn't found in the system. Abort."
+	exit 1
+fi
+
+echo "OK."
+echo "Build the project."
+
+cd CarRentalService/
+mvn clean package
+
+cd ../
+
+docker-compose up --build -d
